4e6d34792d8a9d83eba99a056df39d0fa0f21c8b
William Starzyk <wstarzyk@cps.edu>
2023-11-06 14:15:09 -0600

    Initial commit

---
+++ events.js
@@ -0,0 +1,15 @@
+// We have all of these actions below that we wants to tie to an event!
+
+console.log("w");
+let hoverCard = document.querySelector('[data-secret="hover"]');
+// 1. Add an event listener to reveal the image on hover.
+//  - The selector is hoverCard
+//  - The event type is mouseover
+console.log(hoverCard);
+
+// {
+//   let image = this.querySelector(".hidden-image");
+//   image.style.width = "100%";
+//   image.style.height = "100%";
+//   image.style.opacity = "1";
+// }
